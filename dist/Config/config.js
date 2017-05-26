ad23babb68e15538606cd9645a4b9af54720186f
QUDUSKUNLE <quduskunle@gmail.com>
2017-05-26 10:49:47 +0100

    Update dependencies for deployment

---
+++ dist/Config/config.js
@@ -0,0 +1,30 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _firebase = require('firebase');
+
+var _firebase2 = _interopRequireDefault(_firebase);
+
+var _dotenv = require('dotenv');
+
+var _dotenv2 = _interopRequireDefault(_dotenv);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_dotenv2.default.config();
+
+//======================================Firebase Configuration======================================================// 
+var config = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId
+};
+var db = _firebase2.default.initializeApp(config);
+
+exports.default = db; // export db
---
+++ dist/Config/userRoutes.js
@@ -0,0 +1,124 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _express = require("express");
+
+var _express2 = _interopRequireDefault(_express);
+
+var _config = require("../Config/config.js");
+
+var _config2 = _interopRequireDefault(_config);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// ====================================== Import Libraries=========================================================//
+var Router = _express2.default.Router();
+//=================================Homepage Endpoint===================================================================//
+Router.route("/").post(function (req, res) {
+    res.send('Welcome to PostIt-App');
+});
+
+//==================================Sign Up Endpoint=====================================================================//
+Router.route("/user/signup").post(function (req, res) {
+    var email = req.body.email,
+        password = req.body.password,
+        username = req.body.username;
+    _config2.default.auth().createUserWithEmailAndPassword(email, password).then(function (user) {
+        firebase.database().ref("users").push({
+            userEmail: email,
+            UserPassword: password,
+            userName: username
+        });
+        res.send({
+            message: "Registration successful"
+        });
+    }).catch(function (error) {
+        res.send({
+            message: "error in Registration " + error.message
+        });
+    });
+});
+
+//============================================Sign in Endpoint======================================================//
+
+Router.route("/user/signin").post(function (req, res) {
+    var email = req.body.email,
+        password = req.body.password;
+    _config2.default.auth().signInWithEmailAndPassword(email, password).then(function (user) {
+        res.send({
+            message: "User Signed in successfully"
+        });
+    }).catch(function (error) {
+        res.send({
+            message: error.code + " " + error.message
+        });
+    });
+});
+// ===========================================Sign Out Endpoint===================================================//
+
+Router.route("/user/signout").post(function (req, res) {
+    _config2.default.auth().signOut().then(function () {
+        res.send({
+            message: "Sign-out successful."
+        });
+    }).catch(function (error) {
+        res.send({
+            message: "Try again"
+        });
+    });
+});
+
+//=====================================Create Group Endpoint=======================================================//
+
+Router.route("/group").post(function (req, res) {
+    var email = req.body.email,
+        password = req.body.password,
+        groupName = req.body.group;
+    _config2.default.auth().signInWithEmailAndPassword(email, password).then(function (user) {
+        _config2.default.auth().onAuthStateChanged(function (user) {
+            var uSer = firebase.auth().currentUser,
+                uid = uSer.uid;
+            if (uSer !== null) {
+                var group = groupName.toLowerCase();
+                _config2.default.database().ref("Group").child(group).once("value", function (snapshot) {
+                    if (snapshot.val() != null) {
+                        res.json({ message: "Group already exists" });
+                    } else {
+                        _config2.default.database().ref("Group").child(group).push({
+                            member: uid
+                        });
+                        res.send({ message: "Group Created Successfully" });
+                    }
+                });
+            }
+        });
+    });
+});
+
+//=========================================ADD MEMBER ENDPOINT===============================================//
+
+Router.route('/group/groupId/user').post(function (req, res) {
+    var email = req.body.email,
+        password = req.body.password,
+        groupName = req.body.groupname,
+        groupMember = req.body.user,
+        groupId = req.body.groupId;
+
+    _config2.default.auth().signInWithEmailAndPassword(email, password).then(function (user) {
+        var name = groupName.toLowerCase();
+        _config2.default.database().ref("Group/" + name).push({
+            member: groupMember
+        });
+        res.send({
+            message: "Member added successfully"
+        });
+    }).catch(function (error) {
+        res.send({
+            message: "Ouch!!! Not an authenticated User"
+        });
+    });
+});
+exports.default = Router; // Export apiRouter
