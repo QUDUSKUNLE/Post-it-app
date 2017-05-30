23d2059a830c5247d1a67402843d3221f744a258
QUDUSKUNLE <quduskunle@gmail.com>
2017-05-30 16:58:02 +0100

    Add User's delete Account route

--- server/Config/userRoutes.js
+++ server/Config/userRoutes.js
@@ -19,18 +19,23 @@ Router.route('/user/signup')
         firebase.auth().createUserWithEmailAndPassword(email, password)
             .then(() => {
                 const user = firebase.auth().currentUser;
+                const userId = user.uid;
                 user.sendEmailVerification()
                     .then(() => {
-                        // Send out verification email to a just registered user
-                    });
-                db.database().ref('users').push({
-                    userEmail: email,
-                    UserPassword: password,
-                    userName: username
-                });
-                res.send({
-                    message: 'Registration successful and verification email sent to your email'
-                });
+                        db.database().ref(`users/${userId}`).push({
+                            userEmail: email,
+                            UserPassword: password,
+                            userName: username
+                        });
+                        res.send({
+                            message: 'Registration successful and verification email sent to your email'
+                        });
+                    })
+                    .catch((error) => {
+                        res.status(404).send({
+                            message: 'Network Error'
+                        });
+                    })
             })
             .catch(() => {
                 res.status(502).send({
@@ -67,8 +72,8 @@ Router.route('/user/signout')
                 });
             })
             .catch(() => {
-                res.send({
-                    message: 'Try again'
+                res.status(404).send({
+                    message: 'Network Error'
                 });
             });
     });
@@ -87,12 +92,12 @@ Router.route('/group')
                     const uid = uSer.uid;
                     if (uSer !== null) {
                         const group = groupName.toLowerCase();
-                        db.database().ref('Group').child(group)
+                        db.database().ref(`Group/${uid}`).child(group)
                             .once('value', (snapshot) => {
                                 if (snapshot.val() != null) {
                                     res.status(502).send({ message: 'Group already exists' });
                                 } else {
-                                    db.database().ref('Group').child(group).push({
+                                    db.database().ref(`Group/${uid}`).child(group).push({
                                         member: uid
                                     });
                                     res.send({ message: 'Group Created Successfully' });
@@ -103,7 +108,7 @@ Router.route('/group')
             })
             .catch(() => {
                 res.status(401).send({
-                    message: 'User not registered'
+                    message: 'User`s not registered'
                 });
             });
     });
@@ -118,8 +123,9 @@ Router.route('/group/groupId/user')
         const groupMember = req.body.user;
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(() => {
+                const uSer = (firebase.auth().currentUser).uid;
                 const name = groupName.toLowerCase();
-                db.database().ref(`Group/${name}`).push({
+                db.database().ref(`Group/${uSer}`).child(name).push({
                     member: groupMember
                 });
                 res.send({
@@ -132,6 +138,36 @@ Router.route('/group/groupId/user')
                 });
             });
     });
+
+//	===============Delete a user`s Account============
+Router.route('/user/delete')
+    .post((req, res) => {
+        const email = req.body.email;
+        const password = req.body.password;
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(() => {
+                const user = firebase.auth().currentUser
+                const userId = user.uid;
+                db.database().ref(`users/${userId}`).remove();
+                user.delete()
+                    .then(() => {
+                        res.send({
+                            message: 'User`s deleted successfully '
+                        });
+                    })
+                    .catch((error) => {
+                        res.status(404).send({
+                            message: 'Network Error'
+                        });
+                    });
+            })
+            .catch(() => {
+                res.status(404).send({
+                    message: 'User`s not Found'
+                });
+            });
+    });
+
 
 // Export apiRouter
 export default Router;
