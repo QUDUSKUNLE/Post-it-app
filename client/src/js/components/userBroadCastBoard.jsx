893b093a21f71ce968a4a211c29ab3404ad10b5b
QUDUSKUNLE <quduskunle@gmail.com>
2017-06-28 06:41:21 +0100

    design broadcast message board

--- client/src/js/components/userBroadCastBoard.jsx
+++ client/src/js/components/userBroadCastBoard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import '../../css/icon.css';
 
 /**
  * Represents BroadCastBoard Component.
@@ -12,9 +13,40 @@ class BroadCastBoard extends React.Component {
      */
   constructor(props) {
     super(props);
+    this.state = {
+      message: ''
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+		// Bind signOut tab to onClick event
     this.onClick = this.onClick.bind(this);
   }
 
+	/**
+ * onChange event.
+ * @param {object} message The first number.
+ * @returns {void} bind input values to name.
+ */
+  onChange(message) {
+    this.setState({
+      [message.target.name]: message.target.value
+    });
+  }
+
+	/**
+ * onSubmit event.
+ * @param {object} message .
+ * @returns {void} .
+ */
+  onSubmit(message) {
+    message.preventDefault();
+    const broadcastmessage = {
+      message: this.state.message
+    };
+    axios.post('/groupName/message', broadcastmessage).then(() => {
+      alert('message sent');
+    });
+  }
 	/**
  * onClick event.
  * @param {void} nill no parameter.
@@ -23,11 +55,10 @@ class BroadCastBoard extends React.Component {
   onClick() {
     axios.post('/signout').then((response) => {
       alert(response.data.message);
-				// console.log(response.data);
       this.props.history.push('/');
     }).catch((error) => {
       if (error.response) {
-					// console.log(error.response.data);
+			// console.log(error.response.data);
       }
     });
   }
@@ -75,16 +106,68 @@ class BroadCastBoard extends React.Component {
 								<li role="presentation" data-toggle="modal"
 									data-target="#myModal"><Link to="/group">Create Group</Link>
 								</li>
-								<li role="presentation"><Link to="/member">Add member</Link>
-							</li>
+								<li role="presentation"><Link to="/member">
+								Add member</Link></li>
 							</ul>
 						</div>
 					</div>
-					<div className="row">
-						<div className="col-md-3">
+				</div>
+				<div className="container">
+					<div className="col-md-12">
+						<div className="row board">
+							<div className="col-md-3"
+								style={{ backgroundColor: '#756d6d' }}>
+								<h5 className="text-center para">Groups</h5>
+								<hr />
+								<br />
+								<div className="row">
+                  <div className="col-md-12 col-xs-12 col-lg-12 broadcastbody">
+                    <ul>
+                      <li>Andela Lagos</li>
+											<li>Andela Abuja</li>
+											<li>Andela Calabar</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<div className="col-md-6"
+								style={{ backgroundColor: '#2d2d2d' }}>
+								<h5 className="text-center para">Group: Andela Abuja</h5>
+								<hr />
+								<br />
+								<div className="row">
+                  <div className="col-md-12 col-xs-12 col-lg-12 broadcastbody">
+                    <form id="broadcastform" onSubmit={this.onSubmit}>
+                      <div className="form-group">
+												<input type="text" name="message"
+													onChange={this.onChange} placeholder="write message"
+													className="form-control" value={this.state.message} />
+											</div>
+											<button type="submit"
+												className="btn btn-primary pull-right">Send</button>
+										</form>
+									</div>
+								</div>
+							</div>
+							<div className="col-md-3"
+								style={{ backgroundColor: '#756d6d' }}>
+								<h5 className="text-center para">Group member</h5>
+								<hr />
+								<br />
+								<div className="row">
+                  <div className="col-md-12 col-xs-12 col-lg-12 broadcastbody">
+                    <div>
+											<ul>
+												<li>ebuka</li>
+												<li>quduskunle</li>
+												<li>osayamen</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+							<hr />
 						</div>
-						<div className="col-md-6"></div>
-						<div className="col-md-3"></div>
 					</div>
 				</div>
 			</div>
