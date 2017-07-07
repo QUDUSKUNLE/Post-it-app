454903f7e5df97c5e91b6f147527a43b4712ebd9
QUDUSKUNLE <quduskunle@gmail.com>
2017-07-07 17:46:53 +0100

    Initialise firebase for Google+ sign in

---
+++ client/src/js/vendors/vendors.jsx
@@ -0,0 +1,10 @@
+
+const config = {
+  apiKey: 'AIzaSyBUL7H7Vull9iAEuPIvI1C2yXHHT2hyf7w',
+  authDomain: 'post-it-app.firebaseapp.com',
+  databaseURL: 'https://post-it-app.firebaseio.com',
+  projectId: 'post-it-app',
+  storageBucket: 'post-it-app.appspot.com',
+  messagingSenderId: '465033980113'
+};
+export default config;
