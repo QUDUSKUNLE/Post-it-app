67fed794ee272ebfab7dd6ba336b3ecb90c723d4
QUDUSKUNLE <quduskunle@gmail.com>
2017-07-17 16:01:04 +0100

    Refactor test branch

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const cookieParser = require("cookie-parser");
-const Router = require('./Config/userRoutes.js');
+// const cookieParser = require('cookie-parser');
+const Router = require('./config/userRoutes.js');
 const config = require('config');
 
 // PORT
@@ -15,21 +15,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // configure app to handle CORS requests
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POSTS');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, \
-	content-type, Authorization');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POSTS');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,' +
+	'content-type, Authorization');
+  next();
 });
 // MIDDLEWARE
 if (config.util.getEnv('NODE_ENV') !== 'test') {
-    app.use(morgan('dev')); // log all requests to the console
+  app.use(morgan('dev')); // log all requests to the console
 }
 
 // Register our routes - all routes
 app.use('/', Router);
 
 app.listen(port);
-console.log('port: ' + port);
+console.log(`port: ${port}`);
 
 module.exports = app;
--- test/test.js
+++ test/test.js
@@ -1,192 +1,197 @@
 process.env.NODE_ENV = 'test';
 const app = require('../server/server');
 const chai = require('chai');
-const chaiHttp = require('chai-http');
+// const chaiHttp = require('chai-http');
 const request = require('supertest');
 const expect = chai.expect;
 
 // PostIt-app APIs Unit tests
 describe('PostIt-app:', () => {
-    //	Unit test for the App Homepage route
-    describe('', () => {
-        it('welcome user`s to its page', (done) => {
-            request(app)
-                .get('/')
-                .set('Accept', 'application/json')
-                .end((err, res) => {
-                    expect(res.statusCode).to.equal(200);
-                    if (err) return done(err);
-                    done();
-                });
-        });
+  describe('', () => {   //	Unit test for the App Homepage route
+    it('welcome user`s to its page', (done) => {
+      request(app)
+      .get('/')
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        if (err) return done(err);
+        done();
+      });
     });
+  });
 
-    // Unit test for User`s sign up route
-    describe('Sign Up route', () => {
-        it('should throw `statusCode 200` for every user`s who has registered', (done) => {
-            request(app)
-                .post('/user/signup')
-                .send({
-                    'username': 'Qudus Kunle',
-                    'email': 'quduskunle@gmail.com',
-                    'password': 'kawtharadejoke08971'
-                })
-                .set('Accept', 'application/json')
-                .end((err, res) => {
-                    expect(res.statusCode).to.equal(200);
-                    expect(res.body.message).to.equal('Registration successful and verification email sent to your email');
-                    expect(typeof res.body.message).to.equal('string');
-                    expect(typeof res.body).to.equal('object');
-                    if (err) return done(err);
-                    done();
-                });
-        });
-    });
-
-    // Unit test for Use`s sign in route
-    describe('sign in route', () => {
-        it('should sign in a user`s who had signed up and return `User Signed in successfully`', (done) => {
-            request(app)
-                .post('/user/signin')
-                .send({
-                    'email': 'quduskunle@gmail.com',
-                    'password': 'kawtharadejoke08971'
-                })
-                .set('Accept', 'application/json')
-                .end((err, res) => {
-                    expect(res.statusCode).to.equal(200);
-                    expect(res.body.message).to.equal('User Signed in successfully');
-                    expect(typeof res.body.message).not.be.a('number');
-                    if (err) return done(err);
-                    done();
-                });
-        });
-
-        it('should throw `Not Found` and statusCode `404` if a user`s log in details is not found', (done) => {
-            request(app)
-                .post('/user/signin')
-                .send({
-                    'email': 'kunkebon@gmail.com',
-                    'password': 'kunke08971'
-                })
-                .set('Accept', 'application/json')
-                .end((err, res) => {
-                    expect(res.statusCode).to.equal(404);
-                    expect(res.body.message).to.equal('Not Found');
-                    if (err) return done(err);
-                    done();
-                })
+  describe('Sign Up route', () => {
+    it('should throw `statusCode 200` for every user`s who has registered',
+    (done) => {
+      request(app)
+        .post('/signup')
+        .send({
+          username: 'Qudus Kunle',
+          email: 'quduskunle@gmail.com',
+          password: 'kawtharadejoke08971'
         })
+        .set('Accept', 'application/json')
+          .end((err, res) => {
+            expect(res.statusCode).to.equal(200);
+            expect(res.body.message).to.equal(
+              'Registration successful and verification email sent to your mail'
+            );
+            expect(typeof res.body.message).to.equal('string');
+            expect(typeof res.body).to.equal('object');
+            if (err) return done(err);
+            done();
+          });
     });
+  });
+
+// Unit test for Use`s sign in route
+  describe('sign in route', () => {
+    it('should sign in a user`s who had signed up and return' +
+    ' `User Signed in successfully`', (done) => {
+      request(app)
+      .post('/signin')
+      .send({
+        email: 'quduskunle@gmail.com',
+        password: 'kawtharadejoke08971'
+      })
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body.message).to.equal('User Signed in successfully');
+        expect(typeof res.body.message).not.be.a('number');
+        if (err) return done(err);
+        done();
+      });
+    });
+
+    it('should throw `Not Found` and statusCode `404`' +
+    ' if a user`s log in details is not found', (done) => {
+      request(app)
+      .post('/signin')
+      .send({
+        email: 'kunkebon@gmail.com',
+        password: 'kunke08971'
+      })
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(404);
+        expect(res.body.message).to.equal('Not Found');
+        if (err) return done(err);
+        done();
+      });
+    });
+  });
 
     // Unit test for User`s sign-out route
-    describe('sign out', () => {
-        it('every user`s that signed out with `statusCode 200`', (done) => {
-            request(app)
-                .post('/user/signout')
-                .end((err, res) => {
-                    expect(res.statusCode).to.equal(200);
-                    expect(res.body.message).to.equal('User`s signed-out successfully.');
-                    if (err) return done(err);
-                    done();
-                });
-        });
+  describe('sign out', () => {
+    it('every user`s that signed out with `statusCode 200`', (done) => {
+      request(app)
+      .post('/user/signout')
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body.message).to.equal('User`s signed-out successfully.');
+        if (err) return done(err);
+        done();
+      });
     });
+  });
 
-    //	Unit test for a registered User`s to create a group 
-    describe('allows', () => {
-        it('registered user`s to create groups and return `Group Created Successfully`', (done) => {
-            request(app)
-                .post('/group')
-                .send({
-                    'email': 'quduskunle@gmail.com',
-                    'password': 'kawtharadejoke08971',
-                    'group': 'ilaa' // Change group name
-                })
-                .end((err, res) => {
-                    expect(res.statusCode).to.equal(200);
-                    expect(res.body.message).to.equal('Group Created Successfully');
-                    if (err) return done(err);
-                    done();
-                });
-        });
-
+  //	Unit test for a registered User`s to create a group
+  describe('allows', () => {
+    it('registered user`s to create groups and return' +
+    '`Group Created Successfully`', (done) => {
+      request(app)
+      .post('/group')
+      .send({
+        email: 'quduskunle@gmail.com',
+        password: 'kawtharadejoke08971',
+        group: 'ilaa' // Change group name
+      })
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body.message).to.equal('Group Created Successfully');
+        if (err) return done(err);
+        done();
+      });
     });
+  });
 
     //	Unit test for a non registered user`s
-    describe('should not allow', () => {
-        it('non registered user`s to create groups and return `User`s not registered`', (done) => {
-            request(app)
-                .post('/group')
-                .send({
-                    'email': 'kunle@gmail.com',
-                    'password': 'kawtharadejoke08971',
-                    'group': 'okoko'
-                })
-                .end((err, res) => {
-                    expect(res.statusCode).to.equal(401);
-                    expect(res.body.message).to.equal('User`s not registered');
-                    if (err) return done(err);
-                    done();
-                });
+  describe('should not allow', () => {
+    it('non registered user`s to create groups and return' +
+    ' `User`s not registered`', (done) => {
+      request(app)
+        .post('/group')
+        .send({
+          email: 'kunle@gmail.com',
+          password: 'kawtharadejoke08971',
+          group: 'okoko'
+        })
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(401);
+          expect(res.body.message).to.equal('User`s not registered');
+          if (err) return done(err);
+          done();
         });
     });
+  });
 
     // Unit test for User`s to add member to his/her group
-    describe('should allow registered user`s', () => {
-        it('to add registered friends to his/her group and should return `statusCode 200` and `Member added successfully`', (done) => {
-            request(app)
-                .post('/group/groupId/user')
-                .send({
-                    'email': 'quduskunle@gmail.com',
-                    'password': 'kawtharadejoke08971',
-                    'group': 'okoko',
-                    'user': 'aALLKIiiiw0908erQERRCRTY'
-                })
-                .end((err, res) => {
-                    expect(res.statusCode).to.equal(200);
-                    expect(res.body.message).to.equal('Member added successfully');
-                    if (err) return done(err);
-                    done();
-                });
-        });
+  describe('should allow registered user`s', () => {
+    it('to add registered friends to his/her group and should return' +
+    ' `statusCode 200` and `Member added successfully`', (done) => {
+      request(app)
+      .post('/group/groupId/user')
+      .send({
+        email: 'quduskunle@gmail.com',
+        password: 'kawtharadejoke08971',
+        group: 'okoko',
+        user: 'aALLKIiiiw0908erQERRCRTY'
+      })
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body.message).to.equal('Member added successfully');
+        if (err) return done(err);
+        done();
+      });
     });
+  });
 
     // Unit test for User`s to delete his/her account from PostIt-app
-    describe('should tell', () => {
-        it('non registered user`s `User`s not Found` and return `statusCode 404`', (done) => {
-            request(app)
-                .post('/user/delete')
-                .send({
-                    'email': 'quduskle@gmail.com',
-                    'password': 'kawtharadejoke08971',
-                })
-                .end((err, res) => {
-                    expect(res.statusCode).to.equal(404);
-                    expect(res.body.message).to.equal('User`s not Found');
-                    if (err) return done(err);
-                    done();
-                });
-        });
-
+  describe('should tell', () => {
+    it('non registered user`s `User`s not Found` and return' +
+    ' `statusCode 404`', (done) => {
+      request(app)
+      .post('/user/delete')
+      .send({
+        email: 'quduskle@gmail.com',
+        password: 'kawtharadejoke08971',
+      })
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(404);
+        expect(res.body.message).to.equal('User`s not Found');
+        if (err) return done(err);
+        done();
+      });
     });
+  });
 
     // Unit test for User`s to delete his/her account from PostIt-app
-    describe('should allow', () => {
-        it('registered user`s to delete his/her account and should return `statusCode 200` and `User`s deleted successfully`', (done) => {
-            request(app)
-                .post('/user/delete')
-                .send({
-                    'email': 'quduskunle@gmail.com',
-                    'password': 'kawtharadejoke08971',
-                })
-                .end((err, res) => {
-                    expect(res.statusCode).to.equal(200);
-                    expect(res.body.message).to.equal('User`s deleted successfully ');
-                    if (err) return done(err);
-                    done();
-                });
-        });
-
+  describe('should allow', () => {
+    it('registered user`s to delete his/her account and should return' +
+    ' `statusCode 200` and `User`s deleted successfully`', (done) => {
+      request(app)
+      .post('/user/delete')
+      .send({
+        email: 'quduskunle@gmail.com',
+        password: 'kawtharadejoke08971',
+      })
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body.message).to.equal('User`s deleted successfully ');
+        if (err) return done(err);
+        done();
+      });
     });
+  });
 });
