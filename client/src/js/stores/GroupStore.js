098ba86dad0a87e662d3ed02b58f140592e02d4d
Andela Developer <andeladeveloper@Andelas-MacBook-Pro-5.local>
2017-09-03 21:05:50 +0100

    create GroupStore.js file

---
+++ client/src/js/stores/GroupStore.js
@@ -0,0 +1,39 @@
+import { EventEmitter } from 'events';
+import AppDispatcher from '../dispatcher/AppDispatcher.js';
+
+import {
+  GET_GROUPS,
+  CREATE_NEW_GROUP } from '../constants/ActionConstants.js';
+
+class GroupStore extends EventEmitter {
+  constructor() {
+    super();
+    this.groups = [];
+    this.allGroups = this.allGroups.bind(this);
+    this.handleActions = this.handleActions.bind(this);
+  }
+
+  allGroups() {
+    return this.groups;
+  }
+
+  handleActions(action) {
+    switch (action.type) {
+      case GET_GROUPS:
+        this.groups = action.groups;
+        this.emit('GET_GROUPS');
+        break;
+      case CREATE_NEW_GROUP:
+        this.groups.push(action.groups);
+        this.emit('CREATE_NEW_GROUP');
+        break;
+      default:
+    }
+  }
+}
+
+const groupStore = new GroupStore();
+
+AppDispatcher.register(groupStore.handleActions.bind(groupStore));
+
+export default groupStore;
