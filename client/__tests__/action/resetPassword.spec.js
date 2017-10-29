a50f49d981a38489e993e7384f04499281c2db06
Andela Developer <andeladeveloper@Andelas-MacBook-Pro-10.local>
2017-10-29 06:04:48 +0100

    feature(unit-test)
    - write component test

--- client/__tests__/action/resetPassword.spec.js
+++ client/__tests__/action/resetPassword.spec.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 import expect from 'expect';
 import AppDispatcher from '../../src/dispatcher/AppDispatcher';
 import '../../src/__mock__/firebaseMock.js';
-import passwordResetError from '../../src/__mock__/passwordResetError.json';
 import { resetPassword } from '../../src/actions/resetPasswordActions';
 
 describe('resetPasswordAction', () => {
@@ -36,44 +35,8 @@ describe('resetPasswordAction', () => {
           expect(res).toBeInstanceOf(Object);
         });
         expect(dispatchSpy.calledOnce).toEqual(true);
-        expect(dispatchSpy.getCall(0).args[0].type).toBe('PASSWORD_RESET_SUCCESS');
-      });
-    });
-  });
-});
-
-
-describe('resetPasswordAction', () => {
-  let mockAxios;
-  let dispatchSpy;
-
-  beforeEach(() => {
-    mockAxios = sinon.stub(axios, 'post').callsFake(() => {
-      if (passwordResetError.response) {
-        const error = passwordResetError.response;
-        return Promise.reject({ error });
-      }
-    });
-    dispatchSpy = sinon.spy(AppDispatcher, 'dispatch');
-  });
-
-  afterEach(() => {
-    axios.post.restore();
-    AppDispatcher.dispatch.restore();
-  });
-
-  describe('Test for resetPasswordAction Method', () => {
-    it('should dispatch an action', () => {
-      const mail = { email: 'qudusk@gmail.com' };
-      resetPassword(mail).then(() => {
-        expect(mockAxios.calledOnce).toBe(true);
-        mockAxios.getCall(0).returnValue.then((res) => {
-          expect(res.error).toEqual(passwordResetError.response);
-          expect(res).toBeInstanceOf(Object);
-        });
-        expect(dispatchSpy.calledOnce).toEqual(true);
-        expect(dispatchSpy.getCall(0).args[0].type)
-          .toBe('PASSWORD_RESET_ERROR');
+        expect(dispatchSpy.getCall(0).args[0].type).toBe('PASSWORD_RESET_' +
+         'SUCCESS');
       });
     });
   });
