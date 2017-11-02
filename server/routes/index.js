2fb497976276586d430ce6e7e4aa14694196334a
abdulquddus <qudus.yekeen@andela.com>
2017-11-02 14:25:26 +0100

    chore(code-refactoring): implement feedback

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,66 +9,66 @@ import MessageController from '../controller/MessageController';
 const Router = express.Router();
 
 /**
- * Router for signup users to the application
+ * Route for signup users to the application
  */
 Router.post('/api/v1/signup', UserController.signUp);
 
 /**
- * Router for signin user to the application
+ * Route for signin user to the application
  * Both { email and password }
  */
 Router.post('/api/v1/signin', UserController.signIn);
 
 /**
- * Router for signin user to the application via Google account
+ * Route for signin user to the application via Google account
  * Both { email and password }
  */
 Router.post('/api/v1/google', UserController.googleSignIn);
 
 /**
- * Router for reset user password
+ * Route for reset user password
  * Both { email }
  */
 Router.post('/api/v1/passwordReset', UserController.passwordReset);
 
 /**
- * Router for sign out user from the application
+ * Route for sign out user from the application
  */
 Router.post('/api/v1/signout', UserController.signOut);
 
 /**
- * Router for a signed in user to create groups in the application
+ * Route for a signed in user to create groups in the application
  */
 Router.post('/api/v1/createGroup', GroupController.createGroup);
 
 /**
- * Router to get a user groups
+ * Route to get a user groups
  */
 Router.get('/api/v1/getgroups/:userId', GroupController.getUsersGroups);
 
 /**
- * Router for a registered user to add registered member to a group
+ * Route for a registered user to add registered member to a group
  */
 Router.post('/api/v1/addmember/:groupId', GroupController.addMemberToGroup);
 
 /**
- * Router for a signed user to get members of a group
+ * Route for a signed user to get members of a group
  */
 Router.get('/api/v1/getMembers/:groupId', GroupController.getMembersOfGroup);
 
 /**
- * Router for a signed user to send message to a group
+ * Route for a signed user to send message to a group
  */
 Router.post('/api/v1/sendMessage/:groupId',
   MessageController.sendMessageToGroup);
 
 /**
- * Router for signed user to vew message in a group
+ * Route for signed user to vew message in a group
  */
 Router.get('/api/v1/getMessage/:groupId', MessageController.getMessage);
 
 /**
- * Router for a signed in user to add members to a group
+ * Route for a signed in user to add members to a group
  */
 Router.get('/api/v1/getAllRegisteredUsers',
   GroupController.getAllRegisteredUsers);
