2bdf0c32b5f4f62eb843b23ed7d872081f7e6a87
abdulquddus <qudus.yekeen@andela.com>
2017-11-02 10:14:39 +0100

    [feature component-test] write UserSignIn test

--- client/__tests__/component/userSignIn.spec.js
+++ client/__tests__/component/userSignIn.spec.js
@@ -6,7 +6,6 @@ import { shallow, mount } from 'enzyme';
 import expect from 'expect';
 import sinon from 'sinon';
 import 'babel-polyfill';
-import firebase from '../../src/vendors/index';
 import localStorageMock from '../../src/__mock__/localStorage';
 import axiosMock from '../../src/__mock__/axiosMock.jsx';
 import Footer from '../../src/components/Footer';
@@ -17,33 +16,7 @@ import signInResponse from '../../src/__mock__/signInResponse.json';
 
 
 window.localStorage = localStorageMock;
-// jest.mock('../../src/vendors/index.js', () => {
-//   /**
-//    * @description describes a function that mocks firebase module,
-//    * fires it action to make an Api call, returns a promise that is mocked
-//    *
-//    * @param { void } void takes no parameter
-//    *
-//    * @return { object } mockfirebase object
-//    *
-//    * @function Test
-//    */
-//   function Test() {
-
-//   }
-//   const mockFirebase = jest.fn().mockReturnValue({
-//     signInWithPopup: jest.fn().mockReturnValue(Promise.resolve({
-//       user: 'testUser'
-//     }))
-//   });
-//   Test.prototype.addScope = jest.fn();
-//   mockFirebase.GoogleAuthProvider = Test;
-//   return {
-//     auth: mockFirebase,
-//   };
-// });
-
-describe('UserSignIn component', () => {
+describe('<UserSignIn/>', () => {
   let component;
   let wrapper;
   const mockOnSignIn = sinon.stub(SignInStore,
@@ -52,13 +25,7 @@ describe('UserSignIn component', () => {
     'removeListener').callsFake((user, cb) => cb());
   const mockSignInResponse = sinon.stub(SignInStore,
     'signInUser').returns(signInResponse);
-  // let axiosMock;
-  // const onSubmitSpy = sinon.spy();
-  // const signInActionSpy = signInAction;
   beforeEach(() => {
-    // axiosMock = mockApiCall;
-    // jest.mock('axios', () => axiosMock);
-    // SignInStore.signInUser = jest.fn(() => signInResponse);
     const props = {
       userName: '',
       userId: '',
@@ -97,90 +64,12 @@ describe('UserSignIn component', () => {
       }
     );
   });
-  it('expects UserSignUp component to be defined', () => {
+  it('component to be defined', () => {
     expect(UserSignIn).toBeDefined();
   });
-  it('should render correctly', () => {
+  it('component should render correctly', () => {
     expect(component).toMatchSnapshot();
     expect(component.find('div').length).toBe(15);
   });
-  // it('should have an empty initial state', () => {
-  //   console.log(component);
-  //   // expect(wrapper.state()).toBeDefined();
-  //   // expect(wrapper.state().userName).toEqual('');
-  //   // expect(wrapper.state().userId).toEqual('');
-  //   // expect(wrapper.state().email).toEqual('');
-  //   // expect(wrapper.state().password).toEqual('');
-  // });
-
-  // it('should contain <Footer/>', () => {
-  //   expect(wrapper.find(Footer).root.length).toEqual(1);
-  //   expect(wrapper.find('form').length).toEqual(1);
-  // });
-
-  // it('should contain defined methods', () => {
-  //   expect(wrapper.nodes[0].onChange).toBeDefined();
-  //   expect(wrapper.nodes[0].onSubmit).toBeDefined();
-  //   expect(wrapper.nodes[0].handleSignInAction).toBeDefined();
-  //   expect(wrapper.nodes[0].googleSignIn).toBeDefined();
-  //   expect(wrapper.nodes[0].handleGoogleEvent).toBeDefined();
-  // });
-
-  // it('It should call onChange method', () => {
-  //   // const event = { target: { name: 'name', value: 'value' } };
-  //   // wrapper.instance().onChange(event);
-  //   // expect(wrapper.state().name).toEqual('value');
-  // });
-  // it('should call onSubmit method', () => {
-  //   wrapper.instance().onSubmit({ preventDefault() {} });
-  //   expect(wrapper.state().email).toEqual('');
-  // });
-  // it('should componentDidMount component lifecycle', () => {
-  //   const spy = sinon.spy(UserSignIn.prototype, 'componentDidMount');
-  //   wrapper.instance().componentDidMount();
-  //   expect(spy.calledOnce).toBeTruthy();
-  // });
-
-  // it('should call componentWillUnmount component lifecycle', () => {
-  //   const spy = sinon.spy(UserSignIn.prototype, 'componentWillUnmount');
-  //   wrapper.instance().componentWillUnmount();
-  //   expect(spy.calledOnce).toBeTruthy();
-  // });
-  // it('should find a link', () => {
-  //   console.log(wrapper);
-  //   // expect(wrapper.find(Link).at(2).prop('to')).toEqual('/signin');
-  //   // expect(wrapper.find(Link).at(3).prop('to')).toEqual('/passwordreset');
-  //   // expect(wrapper.find(Link).at(4).prop('to')).toEqual('/');
-  // });
-
-  // it('should render correctly', () => {
-  //   expect(component).toMatchSnapshot();
-  //   expect(component.find('form')).toHaveLength(1);
-  //   expect(component.find('input')).toHaveLength(2);
-  //   expect(component.find('input.signinform')).toHaveLength(2);
-  // });
-  // it('should contain a google button', () => {
-  //   wrapper.find(GoogleButton).simulate('click');
-  //   expect(firebase.auth.GoogleAuthProvider.prototype.addScope)
-  //     .toHaveBeenCalledTimes(1);
-  //   expect(firebase.auth().signInWithPopup).toHaveBeenCalled();
-  // });
-  // it('should take props', () => {
-  //   expect(wrapper.props().onChange).toBeTruthy();
-  //   expect(wrapper.props().onClick).toBeTruthy();
-  //   expect(wrapper.props().handleSignInAction).toBeTruthy();
-  //   expect(wrapper.props().handleGoogleEvent).toBeTruthy();
-  // });
 });
-
-
-// describe('<UserSignIn />', () => {
-//   const signInActionSpy = signInAction;
-//   it('simulates click events', () => {
-//     const onSubmitSpy = sinon.spy();
-//     const component = shallow(<UserSignIn onSubmitSpy={signInActionSpy}/>);
-//     component.find('#signinformbtn').simulate('click');
-//     expect(component.props.onSubmitSpy).toHaveBeenCalled();
-//   });
-// });
 
