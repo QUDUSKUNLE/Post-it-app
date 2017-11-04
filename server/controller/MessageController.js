bd4011075a1aeabb639714a4dd8098369bc223c2
abdulquddus <qudus.yekeen@andela.com>
2017-11-04 03:50:52 +0100

    (chore-code-refactoring) code eslint violation

--- server/controller/MessageController.js
+++ server/controller/MessageController.js
@@ -58,11 +58,16 @@ export default class MessageController {
                 text: 'PostIt-App',
                 html: `
                   <div>
-                    <hr style="height: 0.5px; background-color: white; width: 100%;"/>
+                    <hr
+                      style="height: 0.5px; background-color: white;
+                      width: 100%;"/>
                     <div>
                       <h2 style="margin-top: 25px;">Congratulations!!!</h2>
-                      <p> Good day to you, <br/>&#160;&#160;&#160;&#160;&#160;&#160;You have an <b>important</b> message on PostIt.</p>
-                      <p>Kindly checkout this link to log in and view your message
+                      <p> Good day to you,
+                      <br/>&#160;&#160;&#160;&#160;&#160;&#160;
+                      You have an <b>important</b> message on PostIt.</p>
+                      <p>Kindly checkout this link to log in and view your 
+                      message
                         <a href="https://heroku-postitapp.herokuapp.com/signin">
                           https://heroku-postitapp.herokuapp.com/signin
                         </a>.
@@ -72,7 +77,8 @@ export default class MessageController {
                       <h5>Truly yours,</h5>
                       <p><b>The PostIt Team</b></p>
                     </div>
-                    <hr style="height: 0.5px; background-color: white; width: 100%;"/>
+                    <hr style="height: 0.5px; background-color: white;
+                    width: 100%;"/>
                   </div>`
               };
               transporter.sendMail(mailOptions, (error, info) => {
@@ -101,17 +107,24 @@ export default class MessageController {
                     subject: 'You have a new Message',
                     text: 'PostIt-App',
                     html: `<div>
-                      <hr style="height: 0.5px; background-color: white; width: 100%;" />
+                      <hr style="height: 0.5px; background-color: white;
+                      width: 100%;" />
                       <div>
                         <h2 style="margin-top: 25px;">Congratulations!!!</h2>
-                        <p> Good day to you, <br />&#160;&#160;&#160;&#160;&#160;&#160;You have an <b>important</b> message on PostIt.</p>
-                        <p>Kindly checkout this link to log in and view your message  <a href="https://heroku-postitapp.herokuapp.com/signin">https://heroku-postitapp.herokuapp.com/signin</a>.</p>
+                        <p> Good day to you, 
+                        <br />&#160;&#160;&#160;&#160;&#160;&#160;
+                        You have an <b>important</b> message on PostIt.</p>
+                        <p>Kindly checkout this link to log in and view your 
+                        message  
+                        <a href="https://heroku-postitapp.herokuapp.com/signin">
+                        https://heroku-postitapp.herokuapp.com/signin</a>.</p>
                       </div>
                       <div style="margin-bottom: 15px;">
                         <h5>Truly yours,</h5>
                         <p><b>The PostIt Team</b></p>
                       </div>
-                      <hr style="height: 0.5px; background-color: white; width: 100%;" />
+                      <hr style="height: 0.5px; background-color: white;
+                      width: 100%;" />
                     </div>`
                   };
                   transporter.sendMail(mailOptions, (error, info) => {
