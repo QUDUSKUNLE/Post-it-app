dfc9d72d11b2f866c80c4a981ecda451d4650eaa
abdulquddus <qudus.yekeen@andela.com>
2017-11-18 13:43:01 +0100

    chore(refactor): protect private routes

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom';
 import {
   BrowserRouter as Router,
   browserHistory, Route, Switch } from 'react-router-dom';
-import Routes from './components/Routes.jsx';
-import setAuthToken from '../src/helper/setAuthToken.js';
+import Navbar from './components/Navbar';
+import setAuthToken from '../src/helper/setAuthToken';
 import UserSignIn from './components/UserSignIn';
 import UserCreateGroup from './components/UserCreateGroup';
 import UserBroadCastBoard from './components/UserBroadCastBoard';
-import UserAddMember from './components/UserAddMember';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import UserResetPassword from './components/UserResetPassword';
 import NotFound from './components/NotFound';
+import AuthenticateRoute
+  from '../src/components/Authentication/AuthenticateRoute';
 import './index.scss';
 
 /**
@@ -24,14 +25,19 @@ if (localStorage.token) {
 ReactDOM.render(
   <Router history={browserHistory}>
     <div>
-      <Routes />
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/signin" component={UserSignIn} />
         <Route path="/passwordreset" component={UserResetPassword} />
-        <Route path="/broadcastboard" component={UserBroadCastBoard} />
-        <Route path="/group" component={UserCreateGroup} />
-        <Route path="/member" component={UserAddMember} />
+        <Route
+          path="/broadcastboard"
+          component={AuthenticateRoute(UserBroadCastBoard)}
+        />
+        <Route
+          path="/group"
+          component={AuthenticateRoute(UserCreateGroup)}
+        />
         <Route component={NotFound} />
       </Switch>
       <Footer />
