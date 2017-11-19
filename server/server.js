c0da1ea047bc2935e5ec29ccb3eafd3200bf024d
abdulquddus <qudus.yekeen@andela.com>
2017-11-19 15:34:06 +0100

    chore(Refactor):
    - remove Hot reloading for production

--- server/server.js
+++ server/server.js
@@ -6,12 +6,6 @@ import expressValidator from 'express-validator';
 import morgan from 'morgan';
 import compression from 'compression';
 
-// Noted
-import webpack from 'webpack';
-import webpackMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import config from '../webpack.dev.js';
-
 import router from './routes/router';
 
 const port = process.env.PORT || 8080;
@@ -34,16 +28,6 @@ app.use((req, res, next) => {
 
 // MIDDLEWARE
 app.use(morgan('dev'));
-
-// Added Webpack Noted
-const compiler = webpack(config);
-app.use(webpackMiddleware(compiler, {
-  hot: true,
-  publicPath: config.output.publicPath,
-  noInfo: true
-}));
-
-app.use(webpackHotMiddleware(compiler));
 
 app.use('/', router);
 app.use(express.static(path.join(__dirname, '../client/src/')));
