2c53e1d8e73eba4aaf9d70bb55b4accdefbadb91
abdulquddus <qudus.yekeen@andela.com>
2017-11-23 12:06:04 +0100

    chore(naming-convention):
    - give proper names to helper files

---
+++ server/helper/User.js
@@ -0,0 +1,53 @@
+import dbConfig from '../config/dbConfig';
+
+
+/**
+ * @description This is a class User that contains methods that helps to
+ *  query for user details
+ * from firebase Database
+ *
+ * @class User
+ */
+export default class User {
+
+  /**
+   * @description This function helps get user details from database
+   * @param {string} userId - user Identity
+   * @returns {Object} - user details
+   * @memberof User
+   */
+  static details(userId) {
+    return new Promise((resolve) => {
+      dbConfig.database().ref('users').child(userId).on('value',
+        (snapshot) => {
+          if (snapshot.val()) {
+            resolve(snapshot.val());
+          }
+          resolve({});
+        });
+    });
+  }
+
+  /**
+   * @description This function helps to check if a username exist
+   * @param {string} userName - username
+   * @returns {boolean} - true/false
+   * @memberof User
+   */
+  static checkUser(userName) {
+    return new Promise((resolve) => {
+      dbConfig.database().ref('users/').orderByChild('userName/')
+        .startAt(userName)
+        .endAt(`${userName}\uf8ff`)
+        .once('value', (snapshot) => {
+          let response;
+          if (snapshot.val()) {
+            response = true;
+          } else {
+            response = false;
+          }
+          resolve(response);
+        });
+    });
+  }
+}
